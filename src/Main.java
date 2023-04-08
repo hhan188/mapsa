6e501fa4da3db29a97ded01a588e905d1b76c670
YaserJamali <yaser.jamali@outlook.com>
2023-04-08 22:50:31 +0430

    down triangle matrix

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+
+    static void downTriangleStar(int length) {
+        String[][] strings = new String[length][length];
+
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                strings[i][j] = " ";
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            for (int j = length - 1; j > length - i - 2; j--) {
+                strings[i][j] = "*";
+            }
+        }
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                System.out.print(strings[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        downTriangleStar(20);
+
+    }
+}
+
+
+
