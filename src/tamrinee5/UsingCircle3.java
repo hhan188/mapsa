a6d58a0ff9e8d63ad095e63ac53046dbedf39198
mersedeh <MersedehSadeghifard@yahoo.com>
2023-04-20 23:54:56 +0330

    add practice 7 to git

---
+++ src/tamrinee5/UsingCircle3.java
@@ -0,0 +1,25 @@
+//package tamrinee5;
+//    public class UsingCircle3 {
+//        public static void main(String[] args) {
+//            Circle3[] circles = new Circle3[4];
+//            circles[0] = new Circle3(13);
+//            circles[1] = new Circle3(42);
+//            circles[2] = new Circle3(8);
+//            circles[3] = new Circle3(31);
+//
+//
+//            System.out.println(calculateSumArea(circles));
+//        }
+//
+//
+//        public static double calculateSumArea(Circle3[] circles) {
+//            double sum = 0;
+//            for (Circle3 item : circles) {
+//                sum += item.getArea();
+//
+//
+//            }
+//            return sum;
+//        }
+//    }
+
