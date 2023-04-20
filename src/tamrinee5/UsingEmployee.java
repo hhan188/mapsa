df4c5932b3df1aa86ba4df0e3de559134cab479f
mersedeh <MersedehSadeghifard@yahoo.com>
2023-04-20 23:56:17 +0330

    add practice 7 to git

---
+++ src/tamrinee5/UsingEmployee.java
@@ -0,0 +1,24 @@
+//package tamrinee5;
+//
+//    public class UsingEmployee {
+//        public static void main(String[] args){
+//            Employee[] employeeee=new Employee[3];
+//            employeeee[0]=new Employee(12_000,9);
+//            employeeee[1]=new Employee(9_000,5);
+//            employeeee[2]=new Employee(10_000,6);
+//            System.out.println(bonusGiven(employeeee));
+//
+//
+//
+//        }
+//        public static int bonusGiven(Employee[] employees){
+//            int sum=0;
+//            for (Employee item:employees){
+//                sum+=item.getSalary()* item.getBonus()/100;
+//
+//            }
+//            return sum;
+//        }
+//
+//    }
+
