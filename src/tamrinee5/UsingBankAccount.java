487b6df1cb3a2423394c070156a1b378334fe36d
mersedeh <MersedehSadeghifard@yahoo.com>
2023-04-21 00:00:52 +0330

    add practice 7 to git

---
+++ src/tamrinee5/UsingBankAccount.java
@@ -0,0 +1,22 @@
+//package tamrinee5;
+//
+//public class UsingBankAccount {
+//        public static void main(String[] args){
+//            BankAccount[] accounts=new BankAccount[3];
+//            accounts[0]=new BankAccount(12_000,"13-54");
+//            accounts[1]=new BankAccount(9_000,"19-89");
+//            accounts[2]=new BankAccount(10_000,"10-00");
+//            System.out.println(sumBalance(accounts));
+//
+//
+//
+//        }
+//        public static int sumBalance(BankAccount[] accounts){
+//            int sum=0;
+//            for (BankAccount item:accounts){
+//                sum+=item.getBalance();
+//            }
+//            return sum;
+//        }
+//    }
+
