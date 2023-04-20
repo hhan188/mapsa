ce63c60301872a0a147b201ba5954b575cd2825d
mersedeh <MersedehSadeghifard@yahoo.com>
2023-04-21 00:04:04 +0330

    add practice 7 to git

---
+++ src/tamrinee5/UsingCircle.java
@@ -0,0 +1,9 @@
+//package tamrinee5;
+//
+//public class UsingCircle {
+//        public static void main(String[] args){
+//            Circle circle=new Circle();
+//            System.out.println(circle.getColor("red"));;
+//        }
+//    }
+//
