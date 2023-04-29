ba1dd32d1a23b891a9c03913bab73108a624ce4d
mersedeh <MersedehSadeghifard@yahoo.com>
2023-04-29 18:37:47 +0330

    add practice 7changes to git

--- src/tamrinee5/UsingCar.java
+++ src/tamrinee5/UsingCar.java
@@ -1,11 +1,11 @@
-//package tamrinee5;
-//
-//public class UsingCar {
-//        public static void main(String[] args){
-//            Car car=new Car();
-//            car.startEngine();
-//        }
-//
-//    }
-//
+package tamrinee5;
+
+public class UsingCar {
+        public static void main(String[] args){
+            Car car=new Car();
+            car.startEngine();
+        }
+
+    }
+
 
