afd0b56f325131952f72091cfc40eb5dc9daf7ef
mersedeh <MersedehSadeghifard@yahoo.com>
2023-04-29 18:40:03 +0330

    add practice 7changes to git

--- src/tamrinee5/UsingShape2.java
+++ src/tamrinee5/UsingShape2.java
@@ -1,10 +1,10 @@
 //package tamrinee5;
-//
-//public class UsingShape2 {
-//
-//        public static void main(String[] args){
-//            Square2 square=new Square2("red" ,"yes",9);
-//            System.out.println(square.getArea());
-//        }
-//    }
-//}
+
+public class UsingShape2 {
+
+        public static void main(String[] args){
+            Square2 square=new Square2("red" ,"yes",9);
+            System.out.println(square.getArea());
+        }
+    }
+}
