9a22e74cac141ebea7876e06a3c48558538afcc8
mersedeh <MersedehSadeghifard@yahoo.com>
2023-04-29 18:39:37 +0330

    add practice 7changes to git

--- src/tamrinee5/UsingStudent2.java
+++ src/tamrinee5/UsingStudent2.java
@@ -1,33 +1,33 @@
-//package tamrinee5;
-//
-//public class UsingStudent2 {
-//    package com.mapsahr.practice7;
-//
-//    public class UsingStudent2 {
-//        public static void main(String[] args) {
-//            Student2[] students = new Student2[3];
-//            students[0] = new Student2("zahra", 19);
-//            students[1] = new Student2("sara", 32);
-//            students[2] = new Student2("amir", 17);
-//            System.out.println(highestGpa(students));
-//
-//
-//        }
-//
-//        public static String highestGpa(Student2[] students) {
-//            String highest = students[0].getName();
-//            int highest2 = students[0].getGpa();
-//
-//            for (int i = 0; i < 3; i++) {
-//                if (students[i].getGpa() > highest2) {
-//                    highest2 = students[i].getGpa();
-//                    highest=students[i].getName();
-//                }
-//
-//            }
-//            return highest;
-//
-//        }
-//
-//    }
-//}
+package tamrinee5;
+
+public class UsingStudent2 {
+    package com.mapsahr.practice7;
+
+    public class UsingStudent2 {
+        public static void main(String[] args) {
+            Student2[] students = new Student2[3];
+            students[0] = new Student2("zahra", 19);
+            students[1] = new Student2("sara", 32);
+            students[2] = new Student2("amir", 17);
+            System.out.println(highestGpa(students));
+
+
+        }
+
+        public static String highestGpa(Student2[] students) {
+            String highest = students[0].getName();
+            int highest2 = students[0].getGpa();
+
+            for (int i = 0; i < 3; i++) {
+                if (students[i].getGpa() > highest2) {
+                    highest2 = students[i].getGpa();
+                    highest=students[i].getName();
+                }
+
+            }
+            return highest;
+
+        }
+
+    }
+}
