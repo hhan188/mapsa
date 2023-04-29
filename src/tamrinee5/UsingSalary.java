d9a4afac2f55d354c117f77336e9001263f8fb05
mersedeh <MersedehSadeghifard@yahoo.com>
2023-04-29 18:40:18 +0330

    add practice 7changes to git

--- src/tamrinee5/UsingSalary.java
+++ src/tamrinee5/UsingSalary.java
@@ -1,24 +1,24 @@
-//package tamrinee5;
-//
-//public class UsingSalary {
-//
-//        public static void main(String[] args){
-//            Employee[] employee=new Employee[3];
-//            employee[0]=new Employee(12_000,9);
-//            employee[1]=new Employee(4_000,3);
-//             employee[2]=new Employee(9_000,5);
-//            System.out.println(bonusSalary(employee));
-//
-//        }
-//
-//        public static int bonusSalary(Employee[] employee){
-//            int sum=0;
-//            for (Employee item:employee){
-//                sum+=(item.getSalary()* item.getBonus())/100+ item.getSalary();
-//
-//            }
-//            return sum;
-//        }
-//    }
-//
-//}
+package tamrinee5;
+
+public class UsingSalary {
+
+        public static void main(String[] args){
+            Employee[] employee=new Employee[3];
+            employee[0]=new Employee(12_000,9);
+            employee[1]=new Employee(4_000,3);
+             employee[2]=new Employee(9_000,5);
+            System.out.println(bonusSalary(employee));
+
+        }
+
+        public static int bonusSalary(Employee[] employee){
+            int sum=0;
+            for (Employee item:employee){
+                sum+=(item.getSalary()* item.getBonus())/100+ item.getSalary();
+
+            }
+            return sum;
+        }
+    }
+
+}
