04ea97f98b2881e257d105b90cbb6de96e16ba39
mersedeh <MersedehSadeghifard@yahoo.com>
2023-04-29 18:40:30 +0330

    add practice 7changes to git

--- src/tamrinee5/UsingCircle2.java
+++ src/tamrinee5/UsingCircle2.java
@@ -1,15 +1,15 @@
-//package tamrinee5;
-//
-//public class UsingCircle2 {
-//        public static void main(String[] args){
-//            Circle2[] circle=new Circle2[4];
-//            circle[0]=new Circle2(3);
-//            circle[1]=new Circle2(6);
-//            circle[2]=new Circle2(9);
-//            System.out.println(circle[0].getRadius()+" "+circle[0].circumference());
-//            System.out.println(circle[1].getRadius()+" "+circle[1].circumference());
-//            System.out.println(circle[2].getRadius()+" "+circle[2].circumference());
-//        }
-//    }
-//
+package tamrinee5;
+
+public class UsingCircle2 {
+        public static void main(String[] args){
+            Circle2[] circle=new Circle2[4];
+            circle[0]=new Circle2(3);
+            circle[1]=new Circle2(6);
+            circle[2]=new Circle2(9);
+            System.out.println(circle[0].getRadius()+" "+circle[0].circumference());
+            System.out.println(circle[1].getRadius()+" "+circle[1].circumference());
+            System.out.println(circle[2].getRadius()+" "+circle[2].circumference());
+        }
+    }
+
 
