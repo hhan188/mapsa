f593fb4b65f8fa9ea9e5bf3bd27f318d369dd8bd
YaserJamali <yaser.jamali@outlook.com>
2023-05-28 16:40:09 +0430

    modified and done

--- src/practices/practice15/exercise06/Utils.java
+++ src/practices/practice15/exercise06/Utils.java
@@ -5,13 +5,7 @@ import java.util.*;
 class Utils {
     protected static List<String> getValues(Map<String, String> map) {
         List<String> list = new ArrayList<>(map.values());
-        list.add(map.values().toString());
         Collections.sort(list);
-        for (int i = 0; i < list.size() ; i++) {
-            if (list.get(i).contains("[")||list.get(i).contains("]")){
-                list.remove(i);
-            }
-        }
         return list;
     }
 }
