f60c83e01893fc0235420b9c4d7564e9156c07f3
HomaSiadati <homa.siadati@gmail.com>
2023-06-06 15:54:11 +0330

    This is a test

--- src/Test/test.java
+++ src/Test/test.java
@@ -2,6 +2,6 @@ package Test;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("This is a test for learning GIT");
+        System.out.println("This is a test for learning GIT!");
     }
 }
